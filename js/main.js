7876dc404adc074cfce0376888c9759ea5a85dd4
Sharib <sharib.jafari@baffle.io>
2020-09-16 19:22:05 +0200

    Get rid of complex animations. Speed up console effect speed. Throttle scroll

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
+var scrollCount = 0;
+var scrollThrottleCount = 25;
+
+function contentScroll(){
+    scrollCount++;
+    if(scrollCount > scrollThrottleCount){
+        enable_effects();
+        scrollCount=0;
+    }
+}
+
 function enable_effects() {
     var elements = document.querySelectorAll('.effect:not(.effect-active)');
     elements.forEach(add_active_effect_class);
-
 }
 
 function add_active_effect_class(element, index) {
@@ -10,11 +20,6 @@ function add_active_effect_class(element, index) {
     if (position.top >= 0 && position.bottom <= window.innerHeight) {
         element.classList.add("effect-active");
     }
-
-    // // checking for partial visibility
-    // if (position.top < window.innerHeight && position.bottom >= 0) {
-    //     console.log('Element is partially visible in screen');
-    // }
 }
 
 function activate_effect(card) {
