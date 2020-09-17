7a468654ce9ef2184b76cde9265391005e78725a
Sharib <sharib.jafari@baffle.io>
2020-09-18 01:28:14 +0200

    Re-do console to include cleaner effects

--- js/main.js
+++ js/main.js
@@ -1,31 +1,26 @@
-var scrollCount = 0;
-var scrollThrottleCount = 25;
+let scrollCount = 0;
+const scrollThrottleCount = 25;
+window.onload = function () {
+    enable_effects();
+}
 
-function contentScroll(){
+function contentScroll() {
     scrollCount++;
-    if(scrollCount > scrollThrottleCount){
+    if (scrollCount > scrollThrottleCount) {
         enable_effects();
-        scrollCount=0;
+        scrollCount = 0;
     }
 }
 
 function enable_effects() {
-    var elements = document.querySelectorAll('.effect:not(.effect-active)');
+    const elements = document.querySelectorAll('.effect:not(.effect-active)');
     elements.forEach(add_active_effect_class);
 }
 
 function add_active_effect_class(element, index) {
-    var position = element.getBoundingClientRect();
+    const position = element.getBoundingClientRect();
     // checking whether fully visible
     if (position.top >= 0 && position.bottom <= window.innerHeight) {
         element.classList.add("effect-active");
     }
-}
-
-function activate_effect(card) {
-    card.classList.add("effect-active");
-}
-
-function test() {
-    console.log("test");
 }
