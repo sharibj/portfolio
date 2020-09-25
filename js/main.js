00af35e8b7e29d394d0c60519163da8cdf947b78
sharibj <jafarisharib@gmail.com>
2020-09-25 05:47:27 +0200

    Add 3 more skills. Add alt to all img tags. Show inline svg as experience footer. Change color of inline svg depending on imported theme.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 let scrollCount = 0;
 const scrollThrottleCount = 25;
+
 window.onload = function () {
     enable_effects();
+    load_svg();
 }
 
 function contentScroll() {
@@ -17,10 +19,30 @@ function enable_effects() {
     elements.forEach(add_active_effect_class);
 }
 
-function add_active_effect_class(element, index) {
+function add_active_effect_class(element) {
     const position = element.getBoundingClientRect();
     // checking whether fully visible
     if (position.top >= 0 && position.bottom <= window.innerHeight) {
         element.classList.add("effect-active");
+    }
+}
+
+function load_svg() {
+    const colorElements = document.querySelectorAll("[id$='_color']");
+    console.log(colorElements);
+    colorElements.forEach(setColorToElement)
+    const experienceFooter = document.getElementById('experience_footer');
+    const workingSvg = document.getElementById('working_svg');
+    experienceFooter.innerHTML = workingSvg.innerHTML;
+}
+
+function setColorToElement(itemElement) {
+    const color2 = getComputedStyle(document.documentElement)
+        .getPropertyValue('--color2');
+    itemElement.style.fill = color2;
+    const children = itemElement.children;
+    for (let i = 0; i < children.length; i++) {
+        const child = children[i];
+        child.style.fill = color2;
     }
 }
